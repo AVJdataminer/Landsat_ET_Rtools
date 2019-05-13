b2c395c7b7a5b97351b2950f7b8985543886fb47
Aiden V Johnson <aiden.dataminer@gmail.com>
2019-05-13 16:20:04 -0600

    updated to include a list of zipfiles in different dir

--- CompleteRscripts/unzipper.R
+++ CompleteRscripts/unzipper.R
@@ -3,3 +3,13 @@ temp<-('/Users/aidenvjohnson/Downloads/NLCD2006_landcover_4-20-11_se5.zip')
 unzip(temp, files = NULL, list = FALSE, overwrite = TRUE,
       junkpaths = FALSE, exdir = ".", unzip = "internal",
       setTimes = FALSE)
+
+pathin="C:/downloads"
+pathout="C:/Users/aidenjohnson/Documents/R/win-library/3.5"
+un_data<- list.files(path = pathin, pattern = "zip", all.files = TRUE,
+           full.names = FALSE, recursive = FALSE,
+           ignore.case = FALSE, include.dirs = FALSE)
+len<-length(un_data) 
+files_I_want = un_data[c(1:len)]
+setwd(pathin)
+my_zip = lapply(files_I_want, unzip, exdir = pathout) 
